257f96f1fafee77c9b513be61ac88d7dabb0d858
unknown <doronharitan@gmail.com>
2019-11-05 10:36:20 +0200

    decrease the overfit. saved before I change the network

--- autoencoder.py
+++ autoencoder.py
@@ -12,20 +12,20 @@ class Autoencoder(torch.nn.Module):
             nn.BatchNorm2d(32),
             nn.ReLU(True),
             nn.MaxPool2d(2, stride=2),  # B output (16, 24, 24)
-            nn.Conv2d(32, 64, kernel_size=3, stride=3, padding=0),  # B output (8,8,8)
-            nn.BatchNorm2d(64),
+            nn.Conv2d(32, 16, kernel_size=3, stride=3, padding=0),  # B output (8,8,8)
+            nn.BatchNorm2d(16),
             nn.ReLU(True),
             nn.MaxPool2d(2, stride=2)  # B output (8,4,4)
         )
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2),  # B output (8,9,9)
+            nn.ConvTranspose2d(16, 16, kernel_size=3, stride=2),  # B output (8,9,9)
             nn.ReLU(True),
-            nn.BatchNorm2d(32),
-            nn.ConvTranspose2d(32, 16, kernel_size=3, stride=3, padding=1),  # B output (16,25,25)
             nn.BatchNorm2d(16),
+            nn.ConvTranspose2d(16, 32, kernel_size=3, stride=3, padding=1),  # B output (16,25,25)
+            nn.BatchNorm2d(32),
             nn.ReLU(True),
-            nn.ConvTranspose2d(16, 1, kernel_size=2, stride=2),  # B output (1,50,50)
+            nn.ConvTranspose2d(32, 1, kernel_size=2, stride=2),  # B output (1,50,50)
             # nn.ReLU(True),
             nn.Tanh()
         )
