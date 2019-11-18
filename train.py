874014defa67acfd0d0b38b061c37313e1c96b40
unknown <doronharitan@gmail.com>
2019-11-18 16:56:23 +0200

    changed the vizualization function + added FC option + changed the number of filter in each conv layer

--- train.py
+++ train.py
@@ -3,24 +3,23 @@ from utils import *
 from sklearn.model_selection import train_test_split
 import torch
 from torch.utils.data import DataLoader
-from autoencoder import Autoencoder
+from autoencoder_model import Autoencoder
 from torch import nn
 from torch.utils.tensorboard import SummaryWriter
 import os
-
-# todo:
-#Do I have test data? ask Alex
 
 def main():
     # ====== setting the hp parameters =====
     folder_dir, hp_parameters_dict, device = setting_parameters()
     tensorboard_writer = SummaryWriter(folder_dir)
+    torch.manual_seed(hp_parameters_dict['seed'])
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
     # ====== loading the data =========
     with np.load(os.path.join(hp_parameters_dict['dir'], hp_parameters_dict['file_name'])) as training_data:
         images = training_data['images']
-        # todo, change the names of the paramters and the function
-    visualizing_images(images)  # todo - change, for example visualize images
+    # visualizing_images(images, folder_dir)
 
     # ====== data pre-processing ========
     images = images / hp_parameters_dict['max_pix_value']
@@ -36,54 +35,71 @@ def main():
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=hp_parameters_dict['lr'])
 
+    start = time.time()
     # ===== train the model ======
+    output_stack = 0          # would be use later to visualize the images
     for epoch in range(hp_parameters_dict['num_epochs']):
-        train_loss, val_loss = 0.0, 0.0
-        train_loss = train_model(model, dataloader_dict[0], optimizer, criterion)
+        train_loss, loss_reg = train_model(model, dataloader_dict[0], optimizer, criterion)
         if (epoch % hp_parameters_dict['val_check_interval']) == 0:
-            val_loss = test_model(model, dataloader_dict[1], criterion, epoch)
+            val_loss, output_stack = test_model(model, dataloader_dict[1], criterion, output_stack)
             # write to tensorboard
             train_loss_avr = train_loss/len(dataloader_dict[0])
+            loss_reg_avr = loss_reg/ len(dataloader_dict[0])
             val_loss_avr = val_loss/len(dataloader_dict[1])
             tensorboard_writer.add_scalars('train/val loss',
                            {'train_loss': train_loss_avr, 'val loss': val_loss_avr},
                            epoch)
             #print status in console
-            print('%d Epoch: Train Loss %.4f, Validation loss %.4f' %(epoch, train_loss_avr, val_loss_avr))
+            print('%d Epoch: Train Loss %.4f, Validation loss %.4f, reg loss %.4f'
+                  %(epoch, train_loss_avr, val_loss_avr, loss_reg_avr))
+    start_1 = time.time()
+    visualizing_images(images_val, folder_dir,
+                       compared_data=output_stack.squeeze(1).cpu().detach().numpy(), batch_size=hp_parameters_dict['batch_size'],
+                       epoch_interval = hp_parameters_dict['val_check_interval'])
+    end_1 = time.time()
+    print(end_1 - start_1, 'figures')
     tensorboard_writer.close()
-    torch.save(model, folder_dir)
+    torch.save(model, os.path.join(folder_dir, 'model.pth.tar'))
+    end = time.time()
+    print(end-start)
+
 
 def train_model(model, train_images, optimizer, criterion):
-    train_loss = 0.0
+    train_loss, loss_reg = 0.0, 0.0
     model.train()
     for local_batch in train_images:
         optimizer.zero_grad()                                           # clear the gradients of all optimized variables
-        loss = predection_step(model, local_batch, criterion)
+        loss, _ = predection_step(model, local_batch, criterion)
+        # L1_regularization_FC_1 = torch.cat([x.view(-1) for x in model.fc_1.parameters()])
+        # lambda_1 = 0.01
+        # loss_reg += lambda_1 * torch.norm(L1_regularization_FC_1,1)
         train_loss += loss.item()
         loss.backward()  # compute the gradients
         optimizer.step()  # update the weights with the gradients
-    return train_loss
+    return train_loss, loss_reg
 
 
-def test_model(model, test_images, criterion, epoch):
+def test_model(model, test_images, criterion, output_stack):
     val_loss = 0.0
     model.eval()
-    for batch_num, local_batch in enumerate(test_images):
-        loss = predection_step(model, local_batch, criterion, batch_num, epoch, mode='val')
+    for batch_num,local_batch in enumerate(test_images):
+        loss, output = predection_step(model, local_batch, criterion, mode='val')
         val_loss += loss.item()
-    return val_loss
+        if type(output_stack) == int and batch_num == 0:
+            output_stack = output
+        else:
+            output_stack = torch.cat((output_stack, output))
+    return val_loss, output_stack
 
 
-def predection_step(model, dataset, criterion, batch_num=None, epoch=None, mode='train'):
+def predection_step(model, dataset, criterion, mode='train'):
     if mode != 'train':
         with torch.no_grad():
             outputs = model(dataset)
-            visualizing_images(dataset.squeeze(1).cpu().detach().numpy(),
-                               compared_data=outputs.squeeze(1).cpu().detach().numpy(), batch_num=batch_num, epoch=epoch)
     else:
         outputs = model(dataset)
     loss = criterion(outputs, dataset)  # what is the the square distance between each pixel in the original dataset comapre to the output picture
-    return loss
+    return loss, outputs
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -3,22 +3,25 @@ import time
 import os
 import json
 import torch
+import math
 
 def create_hp_dict():
     DIR = r'C:\Users\Doron\Desktop\Autoencoder\train data' # the directory where you can find the train data
     FILE_NAME = 'rat_unaug_db_50pix.npz'                   # The name of the file containing the data
-    BATCH_SIZE = 64                                        # 'mini-batch size
+    BATCH_SIZE = 128                                        # 'mini-batch size
     SEED = 42                                              #
     VAL_SPLIT = 0.2                                        # the split of train and val
-    NUM_EPOCHS = 300                                       # number of total epochs
+    NUM_EPOCHS = 500                                      # number of total epochs
     LR = 1e-3                                              # initial learning rate
-    OPEN_NEW_FOLDER_STATUS = 'debug'                       # open a new folder for saving the run info, if false the info would be saved in the project dir, if debug the info would be saved in debug folder(default:True)
+    OPEN_NEW_FOLDER_STATUS = 'True'                       # open a new folder for saving the run info, if false the info would be saved in the project dir, if debug the info would be saved in debug folder(default:True)
     MAX_PIX_VALUE = 255.0                                  # max pixsel value, would be used for the rescaling of the images
     VAL_CHECK_INTERBAL = 5                                 # Epochs interval between running validation test
+    RUN_GOAL = 'without FC, with bugger kernels'
     hp_parameters = {'dir': DIR , 'file_name': FILE_NAME,
                       'batch_size': BATCH_SIZE, 'seed': SEED, 'val_split': VAL_SPLIT,
                      'num_epochs': NUM_EPOCHS, 'lr': LR, 'open_new_folder_status': OPEN_NEW_FOLDER_STATUS, 'max_pix_value': MAX_PIX_VALUE,
-                     'val_check_interval': VAL_CHECK_INTERBAL}
+                     'val_check_interval': VAL_CHECK_INTERBAL, 'run_goal': RUN_GOAL}
+    # todo addif we wnat to plot all of the figuers
     return hp_parameters
 
 def setting_parameters():
@@ -42,6 +45,7 @@ def open_new_folder(open_folder_status):
     folder_dir = os.path.join(os.getcwd(), folder_name)
     if not os.path.exists(folder_dir):
         os.makedirs(folder_dir)
+        os.makedirs(os.path.join(folder_dir,'Images'))
     return folder_dir
 
 def save_setting_info(hp_parameters, device, folder_dir):
@@ -50,26 +54,45 @@ def save_setting_info(hp_parameters, device, folder_dir):
         json.dump(hp_parameters, f, indent=2)
         json.dump('\n' + str(device), f)
 
-def visualizing_images(images, compared_data=[], batch_num=None, epoch=None):
+def plot_figuers(images, compared_data):
     nrows = 2
     ncols = 5
-    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(10, 10), sharey=True)
     data_len = len(images)
+    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(10, 10), sharey=True)
     for row in range(nrows):
         if (row + 1) % 2 == 0 and len(compared_data) > 0:
             for col in range(ncols):
-                ax[row,col].imshow(compared_data[col], cmap='gray')
+                ax[row, col].imshow(compared_data[col], cmap='gray')
                 ax[row, col].set_title('After ' + str(col))
         elif (row + 1) % 2 == 0:
             for col in range(ncols):
-                ax[row,col].imshow(images[data_len - col - 1], cmap='gray')
+                ax[row, col].imshow(images[data_len - col - 1], cmap='gray')
                 ax[row, col].set_title('Before ' + str(data_len - col - 1))
         else:
             for col in range(ncols):
-                ax[row,col].imshow(images[col], cmap='gray')
+                ax[row, col].imshow(images[col], cmap='gray')
                 ax[row, col].set_title('Before ' + str(col))
-    if len(compared_data) == 0:
-        plt.savefig('untouched_data', bbox_inches="tight")
+
+def visualizing_images(images, folder_dir, compared_data=[], batch_size=None, epoch_interval=None):
+    if len(compared_data) != 0:
+        num_epochs = math.ceil(compared_data.shape[0]/images.shape[0])
+        for epoch in range(num_epochs):
+            epoch_factor = images.shape[0] * epoch
+            for batch_num, batch in enumerate(range(0,images.shape[0],batch_size)):
+                if (batch + batch_size) > images.shape[0]:
+                    end_batch = images.shape[0]
+                else:
+                    end_batch = batch + batch_size
+                start_batch = batch
+                if epoch != num_epochs-1:
+                    continue
+                else:
+                    plot_figuers(images[start_batch:end_batch],
+                             compared_data[start_batch + epoch_factor:end_batch + epoch_factor])
+                    plt.savefig(os.path.join(folder_dir, 'Images', 'before_vs_after_%depoch_%dbatch_num'
+                                         %(epoch * epoch_interval, batch_num)), bbox_inches="tight")
+                    plt.close('all')
     else:
-        plt.savefig('before_vs_after_%dbatch_%depoch'%(batch_num, epoch), bbox_inches="tight")
-        plt.close('all')
+        plot_figuers(images, compared_data)
+        plt.savefig(os.path.join(folder_dir, 'Images', 'row data'), bbox_inches="tight")
+        plt.close()
